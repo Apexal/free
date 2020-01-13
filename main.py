69127be67950fc638b01384527332769f8045072
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-13 00:09:55 -0500

    Classes

---
+++ main.py
@@ -0,0 +1,8 @@
+from course import Course
+from period import Period
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ period.py
@@ -0,0 +1,16 @@
+class Period:
+    def __init__(self, ptype, days, start_time, end_time, location='TBD'):
+        self.start_time
+        self.ptype = ptype
+        self.days = days
+        self.start_time = start_time
+        self.end_time = end_time
+        self.location = location
+    
+    @property
+    def duration(self):
+        # TODO: calculate
+        return -1
+    
+    def __str__(self):
+        return f'{self.ptype} period on days {self.days} at location {self.location} from {self.start_time} to {self.end_time}'
