6dabfebc4601220a76dc1eca44fe0d857241ca0b
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-15 13:03:14 -0500

    Associate periods and CRNs

--- main.py
+++ main.py
@@ -4,24 +4,45 @@ from period import PeriodError
 def main():
     print('Parsing document...')
 
-    periods = []
     skipped = 0
+
+    # {crn: [periods]}
+    periods = {}
 
     tree = parse_schedule_html_file("data/202001.html")
     course_rows = tree.xpath('/html/body/div/div/center/descendant::table/tbody/tr')
+    crn = None
     for tr in course_rows:
         # Remove non period rows
         if len(tr.xpath('th')) > 0:
             continue
         
+        # Get crn if exists
+        try:
+            crn = tr.xpath('td[1]')[0].text_content().strip().split()[0]
+        except:
+            pass
+        
+        # Attempt to parse the row as a Period, if an exception is thrown, the row must not be a valid period and is skipped
         try:
             period = parse_html_period_row(tr)
-            periods.append(period)
-            print(period)
-        except (IndexError, PeriodError) as e:
+
+            if not crn in periods:
+                periods[crn] = []
+            
+            periods[crn].append(period)
+        except (IndexError, PeriodError):
             # Invalid row!
             skipped += 1
 
-    print(f'Done! Found {len(periods)} periods and skipped {skipped}')
+    # Print results
+    for crn in periods:
+        print(crn)
+        for period in periods[crn]:
+            print(period)
+        print()
+
+    print(f'Done! Found {len(periods)} courses and skipped {skipped}')
+
 if __name__ == "__main__":
     main()
--- parsing.py
+++ parsing.py
@@ -10,7 +10,6 @@ def parse_schedule_html_file(filename):
 
   Returns:
   lxml HTML tree
-
   '''
   with open(filename) as f:
     tree = html.fromstring(f.read())
@@ -30,25 +29,27 @@ def get_td_text(tr, index):
   return tr.xpath(f'td[{index}]')[0].text_content().strip()
 
 def parse_html_period_row(tr):
-  # '92556 STSH-1110-01' -> ['92556', 'STSH-1110-01']
-  # [crn, summary] = tr.xpath('td[1]')[0].strip()
-  
+  '''
+  Creates a Period from a table row.
+  '''
+ 
   ptype = get_td_text(tr, 3)
   if len(ptype) < 3:
     ptype = "???"
 
+  # If days are empty, then we do not care about this period.
   days = list(filter(lambda s: len(s) > 0 and s != " ", get_td_text(tr, 6)))
-
   if len(days) == 0:
     raise PeriodError("Period days are empty.")
 
   start_time = get_td_text(tr, 7)
   end_time = get_td_text(tr, 8)
+  location = tr.xpath('td[10]')[0].text_content().strip()
 
   return Period(
     ptype,
     days,
     start_time,
     end_time,
-    tr.xpath('td[10]')[0].text_content().strip()
+    location
   )
