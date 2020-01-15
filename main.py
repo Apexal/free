9bf4265f083263e32c08ed77c2a21dfa8badaed0
Frank Matranga <thefrankmatranga@gmail.com>
2020-01-15 14:28:01 -0500

    Find day periods

--- main.py
+++ main.py
@@ -9,19 +9,23 @@ def main():
 
     # {crn: [periods]}
     periods, skipped = parse_schedule_html_file('data/202001.html')
-
-    # Print results
-    for crn in periods:
-        print(crn)
-        for period in periods[crn]:
-            print(period)
-        print()
+    all_periods = [item for sublist in periods.values() for item in sublist]
+    # # Print results
+    # for crn in periods:
+    #     print(crn)
+    #     for period in periods[crn]:
+    #         print(period)
+    #     print()
 
     print(f'Done! Found {len(periods)} courses and skipped {skipped}')
 
     for day in range(1, 6):
         print(f'Getting periods on day {day}')
 
+        day_periods = sorted(list(filter(lambda p: day in p.days, all_periods)), key=lambda p: p.start_time)
+        for period in day_periods:
+            print(period)
+        print()
 
 if __name__ == "__main__":
     main()
