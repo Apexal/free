5c4a7d3e7c2158ed378837430572aca90c69a8be
Frank Matranga <thefrankmatranga@gmail.com>
2020-02-11 14:41:40 -0500

    Only grab courses from CRN list

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from course import Course
 from parsing import parse_schedule_html_file, parse_html_period_row
+
+def get_crns():
+    with open('data/crns.txt') as f:
+        return set(line.strip() for line in f.readlines())
 
 def main():
     print('Parsing document...')
 
+    crns = get_crns()
     periods = []
 
-    tree = parse_schedule_html_file("data/202001.html")
+    tree = parse_schedule_html_file('data/202001.html')
     course_rows = tree.xpath('/html/body/div/div/center/descendant::table/tbody/tr')
     for tr in course_rows:
         # Remove non period rows
@@ -15,11 +19,15 @@ def main():
         
         try:
             period = parse_html_period_row(tr)
-            periods.append(period)
-        except:
+            if period.crn in crns:
+                print(f'Found course {period.course_title}')
+                periods.append(period)
+        except Exception as e:
+            # print(e)
             # Invalid row!
             pass
 
     print(f'Done! Found {len(periods)} periods')
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
